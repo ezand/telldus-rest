2560ca501b1991f2607eed8ce9a8960dafb29b90
Eirik Stenersen Sand <eiriksand@gmail.com>
2015-04-13 09:22:43 +0200

    Added exception handler for general unhandled exceptions

--- src/main/java/org/ezand/telldus/rest/exception/GlobalExceptionHandler.java
+++ src/main/java/org/ezand/telldus/rest/exception/GlobalExceptionHandler.java
@@ -1,18 +1,30 @@
 package org.ezand.telldus.rest.exception;
 
 import static org.ezand.telldus.rest.dto.Result.fail;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import org.ezand.telldus.rest.dto.Result;
-import org.springframework.http.HttpStatus;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
-	@ResponseStatus(HttpStatus.NOT_FOUND)
+	private static final Logger LOG = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+	@ResponseStatus(NOT_FOUND)
 	@ExceptionHandler(NotFoundException.class)
 	public Result handleNotFound() {
 		return fail();
+	}
+
+	@ResponseStatus(INTERNAL_SERVER_ERROR)
+	@ExceptionHandler(Throwable.class)
+	public Result handleGeneralException(final Throwable t) {
+		LOG.error("Caught a general unhandled exception", t);
+		return fail(t.getMessage());
 	}
 }
